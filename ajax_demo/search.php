12fd1039e7a2c7de96f736d7db85dbcbb608381c
ankitmohla24 <ankitmohla24@gmail.com>
2018-04-06 07:40:55 -0500

    Ajax Practice

---
+++ ajax_demo/search.php
@@ -0,0 +1,21 @@
+<?php
+
+$connection = mysqli_connect('localhost','root','','ajax');
+$search = $_POST['search'];
+if(!empty($search)){
+    $query = "SELECT * FROM cars WHERE carName LIKE '$search%'";
+    $search_query = mysqli_query($connection,$query);
+    if(!$search_query){
+        die('QUERY FAILED'.mysqli_error($connection));
+    }
+    else{
+        echo "<ul class='list-unstyled'>";
+        while($row = mysqli_fetch_array($search_query)){
+            $brand = $row["carName"];
+            echo"<li> {$brand} in stock";
+        }
+        echo "<ul>";
+    }
+}
+
+?>
